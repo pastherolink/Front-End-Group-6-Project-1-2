23609c788b460cdcf088568b5e7209ddeab2df51
Sean Lebida <seanmlebida@cmconnect.cmcc.edu>
2025-03-01 12:06:56 -0500

    Add api.js utility file for API interactions from advise.

---
+++ recipe-frontend/src/utils/api.js
@@ -0,0 +1,64 @@
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000/api';
+
+export const GET = (endpoint) => {
+  return fetch(`${API_URL}${endpoint}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    credentials: 'include'
+  })
+    .then((res) => res.json())
+    .catch((err) => {
+      console.error('Error calling API:', err);
+      throw err;
+    });
+};
+
+export const POST = (endpoint, body) => {
+  return fetch(`${API_URL}${endpoint}`, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    credentials: 'include'
+  })
+    .then((res) => res.json())
+    .catch((err) => {
+      console.error('Error calling API:', err);
+      throw err;
+    });
+};
+
+export const PUT = (endpoint, body) => {
+  return fetch(`${API_URL}${endpoint}`, {
+    method: 'PUT',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    credentials: 'include'
+  })
+    .then((res) => res.json())
+    .catch((err) => {
+      console.error('Error calling API:', err);
+      throw err;
+    });
+};
+
+export const DELETE = (endpoint, body) => {
+  return fetch(`${API_URL}${endpoint}`, {
+    method: 'DELETE',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    credentials: 'include'
+  })
+    .then((res) => res.json())
+    .catch((err) => {
+      console.error('Error calling API:', err);
+      throw err;
+    });
+};
